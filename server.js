8528bb2031bca6d89b5e928b5a89c1bf73335e22
kimrasng <dohyun2008@kakao.com>
2024-07-12 08:52:59 +0900

    chat app

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// const chatRouter = require('./src/chat');
+let nicknames = [];
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/pages/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
+  socket.on('join', (nickname) => {
+    if (nicknames.includes(nickname)) {
+      socket.emit('nickname duplicate');
+    } else {
+      nicknames.push(nickname);
+      socket.nickname = nickname;
+      io.emit('chat message', `${nickname}가 접속함`);
+    }
+  });
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', `[${socket.nickname}]: ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.nickname) {
+      io.emit('chat message', `${socket.nickname}가 나감`);
+      nicknames = nicknames.filter(n => n !== socket.nickname);
+    }
   });
 });
 
-// app.use('/chat', chatRouter);
-
-server.listen(4000, () => {
-  console.log('listening on *:4000');
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });

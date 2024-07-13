5ffcf041778fced57032f0992bac72c63ce5d270
kimrasng <dohyun2008@kakao.com>
2024-07-13 14:52:28 +0900

    asd

--- server.js
+++ server.js
@@ -5,27 +5,29 @@ const server = http.createServer(app);
 const socketio = require('socket.io');
 const sqlite3 = require('sqlite3').verbose();
 const cookieParser = require('cookie-parser');
-
 const db = new sqlite3.Database(':memory:');
+const io = socketio(server);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// databass 테이블 생성
 db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, nickname TEXT, password TEXT)');
 });
 
+// 페이지 연결
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-const io = socketio(server);
-
+// socket 연결
 io.on('connection', (socket) => {
   const sessionId = socket.handshake.headers.cookie && socket.handshake.headers.cookie.split('=')[1];
   socket.sessionId = sessionId;
 
+  // 로그인 기능 구현
   socket.on('login', (username, password) => {
     db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
       if (err) {
@@ -47,6 +49,7 @@ io.on('connection', (socket) => {
     });
   });
 
+  // 회원가입 기능
   socket.on('signup', (username, nickname, password) => {
     db.run('INSERT INTO users (username, nickname, password) VALUES (?, ?, ?)', [username, nickname, password], (err) => {
       if (err) {
@@ -62,16 +65,19 @@ io.on('connection', (socket) => {
     });
   });
 
+  // 채팅 기능
   socket.on('chat message', (msg) => {
     io.emit('chat message', `[${socket.username}] : ${msg}`);
   });
 
+  // 퇴장 기능
   socket.on('disconnect', () => {
     if (socket.username) {
       io.emit('chat message', `${socket.username}님이 나가셨습니다.`);
     }
   });
 
+  // 로그아웃 기능
   socket.on('logout', () => {
     socket.sessionId = null;
     socket.emit('delete cookie', 'sessionId');
